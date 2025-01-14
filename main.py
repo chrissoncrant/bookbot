b2d090da07465097c5bedacf386da157b70103ac
chrissoncrant <christopher.soncrant@gmail.com>
2025-01-14 14:28:27 -0800

    updated count_characters fn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 test_obj = {
-    "string_1": "'shall', 'ascend', 'my', 'funeral', 'pile', 'triumphantly', 'and', 'exult', 'in', 'the', 'agony', 'of', 'the', 'torturing', 'flames.', 'The', 'light'",
+    "string_1": "'Shall', 'ascend', 'my', 'funeral', 'pile', 'triumphantly', 'and', 'exult', 'in', 'tHe', 'agony', 'of', 'the', 'torturing', 'flames.', 'The', 'light'",
+
     "list_1": ['shall', 'ascend', 'my', 'funeral', 'pile', 'triumphantly', 'and', 'exult', 'in', 'the', 'agony', 'of', 'the', 'torturing', 'flames.', 'The', 'light']
 }
 
@@ -12,23 +13,36 @@ def word_count(text):
     words = text.split()
     return len(words)
 
-def count_characters(string, character):
-    lowercase_string = string.lower()
+def count_characters(text, onlyLetters = True):
     character_dictionary = {}
-    for char in character:
+    def counter(char):
+        if char in character_dictionary:
+            character_dictionary[char] += 1
+        else:
+            character_dictionary[char] = 1  
+
+    for char in text:
         char = char.lower()
-        character_dictionary[char] = lowercase_string.count(char)
+        if onlyLetters:
+            if char.isalpha():
+                counter(char)
+        else:
+            counter(char)
     
     return character_dictionary
 
-print(count_characters(test_obj["string_1"], test_obj["list_1"]))
+
+
+# print(count_characters(test_obj["string_1"], False))
 def main():
     text = get_book_text("./books/frankenstein.txt")
 
     words = word_count(text)
 
-    print(f"{words} words in this text.")
+    character_dictionary = count_characters(text)
+
+    print(character_dictionary)
 
 
 
-# main()
+main()
